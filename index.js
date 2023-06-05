9dd136e9d6ebdddcd898d4e2b40e25f6e4c4bc18
Sarang Personal <sarang89@gmail.com>
2023-06-05 21:12:02 +0530

    Changed welcome message

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
-const {uploadImage} = require("./helper");
+const { uploadImage } = require("./helper");
 
 const app = express();
 
 console.log("Hello from inside Cloud Run");
 
-const multerMid = multer({
-});
+const multerMid = multer({});
 
 app.disable("x-powered-by");
-app.use(multerMid.single('file'));
+app.use(multerMid.single("file"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3003;
 
 console.log(process.env.PORT);
 
 app.get("/", (req, res, next) => {
-  res.json("Hey there from inside Cloud Run!");
+  res.json("Hey there from inside Cloud Run using Artifact!");
 });
 
-app.post("/upload-file", async (req, res, next)=>{
+app.post("/upload-file", async (req, res, next) => {
   try {
     let file = req.file;
-  
+
     const publiclUrl = await uploadImage(file);
-  
+
     res.status(200).json({
       message: "Uploaded image successfully",
-      data: publiclUrl
-    })
+      data: publiclUrl,
+    });
   } catch (error) {
     res.status(500).json(error);
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
